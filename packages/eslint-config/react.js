a8d2ed6bd325cf453eea118273bb64ca7c0dd73e
Tomas Meriles <tomasmeriles17@gmail.com>
2026-03-24 15:08:20 -0300

    chore: add typescript config files for nest and react
    - Remove shared ui components
    - Remove docs project (For now)

--- packages/eslint-config/react.js
+++ packages/eslint-config/react.js
@@ -1,26 +1,20 @@
-import js from "@eslint/js";
-import eslintConfigPrettier from "eslint-config-prettier";
-import tseslint from "typescript-eslint";
-import pluginReactHooks from "eslint-plugin-react-hooks";
 import pluginReact from "eslint-plugin-react";
+import pluginReactHooks from "eslint-plugin-react-hooks";
 import globals from "globals";
 import { config as baseConfig } from "./base.js";
 
 /**
- * A custom ESLint configuration for libraries that use React.
+ * A shared ESLint configuration for React applications.
  *
- * @type {import("eslint").Linter.Config[]} */
+ * @type {import("eslint").Linter.Config[]}
+ * */
 export const config = [
   ...baseConfig,
-  js.configs.recommended,
-  eslintConfigPrettier,
-  ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
   {
     languageOptions: {
       ...pluginReact.configs.flat.recommended.languageOptions,
       globals: {
-        ...globals.serviceworker,
         ...globals.browser,
       },
     },
@@ -32,7 +26,6 @@ export const config = [
     settings: { react: { version: "detect" } },
     rules: {
       ...pluginReactHooks.configs.recommended.rules,
-      // React scope no longer necessary with new JSX transform.
       "react/react-in-jsx-scope": "off",
     },
   },
